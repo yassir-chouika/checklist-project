028a4ec0b8c2664640d472f84e4af163d09e3dbc
CHOUIKA Yassir <yassir.chouika@gmail.com>
2024-12-04 15:11:47 +0100

    Add first-time user welcoming piece of UI

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,22 +1,39 @@
 import React from "react";
 import ChecklistCard from "../component/CheckListCard";
-
+import { useNavigate } from "react-router-dom";
 
 const DashBoard = ({ checklists }) => {
-  const handleDelete = () => {
-    alert(`Delete checklist button is working`);
+  const handleDelete = (id) => {
+    alert(`Delete checklist with id: ${id}`);
   };
 
-  const handleModify = () => {
-    alert(`Modify checklist button is working`);
+  const handleModify = (id) => {
+    alert(`Modify checklist with id: ${id}`);
   };
 
-  
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("Form");
+  };
 
   return (
     <main className="flex justify-center flex-col tablet:flex-row tablet:flex-wrap md:flex-row md:flex-wrap gap-4 p-5">
       {checklists.length === 0 ? (
-        <p>No checklists yet</p>
+        <div className="flex flex-col items-center justify-center p-8 bg-gray-50 rounded-lg laptop:mx-40 laptop:my-16  w-full">
+          <img src="../public/Checklist-icon.png" alt="" />
+          <h3 className="text-xl font-bold text-gray-700 mb-2">
+            No Checklists Available
+          </h3>
+          <p className="text-gray-500 mb-4 text-center">
+            Get started by creating your first checklist
+          </p>
+          <button
+            onClick={handleClick}
+            className="px-4 py-2 bg-paletteYellow text-white rounded-lg hover:bg-amber-200 transition-colors font-bold"
+          >
+            Create Checklist
+          </button>
+        </div>
       ) : (
         checklists.map((checklist) => (
           <ChecklistCard
